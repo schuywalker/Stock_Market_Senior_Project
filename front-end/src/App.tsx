c955e1be1cbb296cb4c199caf5d03966b6b1745b
schuyler <sasplin@ewu.edu>
2023-02-09 18:56:27 -0800

    clean up front end, add mui

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/card/Card";
 import Stock from "./components/stock/Stock";
@@ -14,29 +12,17 @@ function App() {
     ];
 
     return (
-        <div className="App">
-            <Card className="card">
-                <Stock name={stocks[0].name} ticker={stocks[0].ticker} />
-                <Stock name={stocks[1].name} ticker={stocks[1].ticker} />
-                <Stock name={stocks[2].name} ticker={stocks[2].ticker} />
-                <Stock name={stocks[3].name} ticker={stocks[3].ticker} />
-                <Stock name={stocks[4].name} ticker={stocks[4].ticker} />
-            </Card>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <div className="App">
+                <Card className="card">
+                    <Stock name={stocks[0].name} ticker={stocks[0].ticker} />
+                    <Stock name={stocks[1].name} ticker={stocks[1].ticker} />
+                    <Stock name={stocks[2].name} ticker={stocks[2].ticker} />
+                    <Stock name={stocks[3].name} ticker={stocks[3].ticker} />
+                    <Stock name={stocks[4].name} ticker={stocks[4].ticker} />
+                </Card>
+            </div>
+        </>
     );
 }
 
