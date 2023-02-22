920eaf77b4b057debd6863ec40375dbaa50714a9
schuyler <sasplin@ewu.edu>
2023-02-22 12:33:15 -0800

    back-end client uses file from secret folder for key

---
+++ back-end/finnhubCalls.py
@@ -0,0 +1,24 @@
+import finnhub as fn
+import requests
+import pandas as pd
+
+#connect with the client
+with open('./secrets/finnhub_api_key.txt') as f:
+    key = f.readline()
+f.close()
+finnhub_client = fn.Client(api_key=key)
+
+res = finnhub_client.stock_candles('AAPL', 'D', 1590988249, 1591852249)
+print(res)
+print("--------------------------------------------------------------------------------")
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+#Convert to Pandas Dataframe
+
+print(pd.DataFrame(res))
+print("--------------------------------------------------------------------------------")
+
+# Basic financials
+basic = finnhub_client.company_basic_financials('AAPL', 'all')
+pd.DataFrame(basic)
