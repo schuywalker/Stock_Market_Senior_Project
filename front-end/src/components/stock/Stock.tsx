0c0c23f28dc425e01f30b163dff0448ec66af7eb
mlorhan <mlorhan@ewu.edu>
2023-02-25 19:26:14 -0800

    Making cards look nicer, color on percent and adding more info

--- front-end/src/components/stock/Stock.tsx
+++ front-end/src/components/stock/Stock.tsx
@@ -1,42 +1,55 @@
 import { useTheme } from "@mui/material";
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { useContext } from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { useContext, useState } from "react";
 import { ColorModeContext, tokens } from "../../theme";
-import { spacing } from '@mui/system';
+import { spacing } from "@mui/system";
 
+const Stock = (props: any) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
 
-const Stock = (props:any) => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    const colorMode = useContext(ColorModeContext);
+  const stockName: string = props.name;
+  const stockTicker: string = props.ticker;
+  const stockLast: number = props.last;
+  const stockPerChg: number = props.perChg;
+  const stockPerChgStatus: string =
+    stockPerChg > 0
+      ? colors.green[500]
+      : stockPerChg === 0
+      ? colors.grey[100]
+      : colors.red[500];
 
-    const stockStatus:string = colors.grey[500];
-    const stockName:string = props.name;
-    const stockTicker:string = props.ticker;
+  const [changeStockColor, setChangeStockColor] = useState("");
 
-    return(
-        <Card sx = {{minWidth: 275, m: '2em', bgcolor: stockStatus}}>
-            <CardContent>
-                <Typography sx = {{fontSize: 14}} color={colors.green[100]} gutterBottom>
-                    
-                </Typography>
-                <Typography variant="h5" component="div">
-                    {stockName} - {stockTicker} 
-                </Typography>
-            </CardContent>
-        </Card>
-    )
-}
+  return (
+    <Card
+      sx={{
+        maxWidth: 275,
+        m: "2em",
+        bgcolor: colors.grey[500],
+      }}
+    >
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 14 }}
+          color={stockPerChgStatus}
+          gutterBottom
+        >
+          {stockPerChg}%
+        </Typography>
+        <Typography sx={{ fontSize: theme.typography.h5 }} component="div">
+          {stockName} - {stockTicker}
+        </Typography>
+        <Typography sx={{ fontSize: theme.typography.h6 }}>
+          Last: {stockLast}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default Stock;
-
-// export default function OutlinedStock(){
-//     return(
-//         <Box sx={{minWidth:275}}>
-//             <Stock variant = "outlined">{Stock}</Stock>
-//         </Box>
-//     );
-// }
--- front-end/src/scenes/watchlist/Watchlist.tsx
+++ front-end/src/scenes/watchlist/Watchlist.tsx
@@ -1,29 +1,81 @@
-import React from "react"
-import Stock from "../../components/stock/Stock"
+import { Card, CardContent, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import React, { useState } from "react";
+import Stock from "../../components/stock/Stock";
 
 const Watchlist = () => {
+  const stocks = [
+    { name: "Apple", ticker: "AAPL", last: 32416.92, perChg: -1.02 },
+    { name: "Microsoft", ticker: "MSFT", last: 11394.94, perChg: 1.29 },
+    { name: "Amazon", ticker: "AMZN", last: 16753.4, perChg: 0.23 },
+    { name: "Facebook", ticker: "FB", last: 2400.16, perChg: -1.53 },
+    { name: "Google", ticker: "GOOG", last: 42064.97, perChg: 0.0 },
+  ];
 
-    const stocks = [
-        { name: "Apple", ticker: "AAPL" },
-        { name: "Microsoft", ticker: "MSFT" },
-        { name: "Amazon", ticker: "AMZN" },
-        { name: "Facebook", ticker: "FB" },
-        { name: "Google", ticker: "GOOG" },
-    ];
+  const [quote, setQuote] = useState("");
+  const [analystCalls, setAnalystCalls] = useState();
 
-    return (
-        
-        <>
-        <Stock name = {stocks[0].name} ticker = {stocks[0].ticker}/>
-        <Stock name = {stocks[1].name} ticker = {stocks[1].ticker}/>
-        <Stock name = {stocks[2].name} ticker = {stocks[2].ticker}/>
-        <Stock name = {stocks[3].name} ticker = {stocks[3].ticker}/>
-        <Stock name = {stocks[4].name} ticker = {stocks[4].ticker}/>
+  const analystCallsHandler = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8080/analystRec", {});
+      const json = await response.json();
 
-        </>
-        
-    )
+      setAnalystCalls(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-}
+  const quoteHandler = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8080/quote", {});
+      const json = await response.json();
+
+      setQuote(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <Stock
+        name={stocks[0].name}
+        ticker={stocks[0].ticker}
+        last={stocks[0].last}
+        perChg={stocks[0].perChg}
+      />
+      <Stock
+        name={stocks[1].name}
+        ticker={stocks[1].ticker}
+        last={stocks[1].last}
+        perChg={stocks[1].perChg}
+      />
+      <Stock
+        name={stocks[2].name}
+        ticker={stocks[2].ticker}
+        last={stocks[2].last}
+        perChg={stocks[2].perChg}
+      />
+      <Stock
+        name={stocks[3].name}
+        ticker={stocks[3].ticker}
+        last={stocks[3].last}
+        perChg={stocks[3].perChg}
+      />
+      <Stock
+        name={stocks[4].name}
+        ticker={stocks[4].ticker}
+        last={stocks[4].last}
+        perChg={stocks[4].perChg}
+      />
+
+      <Button onClick={quoteHandler}>Get quote</Button>
+      <Typography>{quote}</Typography>
+      <Button onClick={analystCallsHandler}>Get analyst calls</Button>
+      <Typography>{analystCalls}</Typography>
+    </>
+  );
+};
 
 export default Watchlist;
