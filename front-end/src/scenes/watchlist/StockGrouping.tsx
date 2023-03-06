aed778f40b48ecfa01f33cad8d247c50b81772a3
mlorhan <mlorhan@ewu.edu>
2023-03-06 14:22:27 -0800

    Making DisplayGroup.tsx and importing to simplify

--- front-end/src/scenes/watchlist/StockGrouping.tsx
+++ front-end/src/scenes/watchlist/StockGrouping.tsx
@@ -1,28 +1,28 @@
 import { Box } from "@mui/material";
 import Stock from "../../components/stock/Stock";
+import DisplayGroup from "./DisplayGroup";
 
-interface displayGroup {
-    name: string;
-    ticker: string;
-    price: number;
-    perChg: number;
-}
+const StockGrouping = (props: { displayGroup: DisplayGroup[] }) => {
+  const stocks = props.displayGroup;
 
-const StockGrouping = (props: { displayGroup: displayGroup[] }) => {
-    const stocks = props.displayGroup;
-
-    return (
-        <Box display="flex" sx={{ mx: "5%" }}>
-            {stocks.map((_stock: any, i: number) => (
-                <Stock
-                    key={i}
-                    name={stocks[i].name}
-                    ticker={stocks[i].ticker}
-                    price={stocks[i].price}
-                    perChg={stocks[i].perChg}
-                />
-            ))}
-        </Box>
-    );
+  return (
+    <Box display="flex" sx={{ mx: "5%" }}>
+      {stocks.map((_stock: any, i: number) => (
+        <Stock
+          key={i}
+          stockName={stocks[i].stockName}
+          stockTicker={stocks[i].stockTicker}
+          price={stocks[i].price}
+          perChange={stocks[i].perChange}
+          earnings={stocks[i].earnings}
+          threeArticles={stocks[i].threeArticles}
+          marketCap={stocks[i].marketCap}
+          peRatio={stocks[i].peRatio}
+          peRatioTTM={stocks[i].peRatioTTM}
+          dividendYield={stocks[i].dividendYield}
+        />
+      ))}
+    </Box>
+  );
 };
 export default StockGrouping;
--- front-end/src/scenes/watchlist/Watchlist.tsx
+++ front-end/src/scenes/watchlist/Watchlist.tsx
@@ -5,110 +5,118 @@ import Button from "@mui/material/Button";
 import { useState } from "react";
 import Carousel from "react-material-ui-carousel";
 import StockGrouping from "./StockGrouping";
+import DisplayGroup from "./DisplayGroup";
 
 const Watchlist = () => {
-    const stocks = [
-        { name: "1Apple", ticker: "AAPL", price: 32416.92, perChg: -1.02 },
-        { name: "2Microsoft", ticker: "MSFT", price: 11394.94, perChg: 1.29 },
-        { name: "3Amazon", ticker: "AMZN", price: 16753.4, perChg: 0.23 },
-        { name: "4Facebook", ticker: "FB", price: 2400.16, perChg: -1.53 },
-        { name: "5Google", ticker: "GOOG", price: 42064.97, perChg: 0.0 },
-        { name: "6Apple", ticker: "AAPL", price: 32416.92, perChg: -1.02 },
-        { name: "7TSLA", ticker: "TSLA", price: 11394.94, perChg: 1.29 },
-        { name: "8TSLA", ticker: "TSLA", price: 16753.4, perChg: 0.23 },
-        { name: "9TSLA", ticker: "TSLA", price: 2400.16, perChg: -1.53 },
-        { name: "10GME", ticker: "yolo", price: 42064.97, perChg: 0.0 },
-        { name: "11AMC", ticker: "obv", price: 32416.92, perChg: -1.02 },
-        { name: "12Wework", ticker: "very hard", price: 11394.94, perChg: 1.29 },
-        { name: "13Meta", ticker: "might go up", price: 16753.4, perChg: 0.23 },
-    ];
+  const stocks = [
+    { name: "1Apple", ticker: "AAPL" },
+    { name: "2Microsoft", ticker: "MSFT" },
+    { name: "3Amazon", ticker: "AMZN" },
+    { name: "4Facebook", ticker: "FB" },
+    { name: "5Google", ticker: "GOOG" },
+    { name: "6Apple", ticker: "AAPL" },
+    { name: "7TSLA", ticker: "TSLA" },
+    { name: "8TSLA", ticker: "TSLA" },
+    { name: "9TSLA", ticker: "TSLA" },
+    { name: "10GME", ticker: "yolo" },
+    { name: "11AMC", ticker: "obv" },
+    { name: "12Wework", ticker: "very hard" },
+    { name: "13Meta", ticker: "might go up" },
+  ];
 
-    const [watchlistPosition, setWatchlistPosition] = useState(0);
-    interface displayGroup {
-        name: string;
-        ticker: string;
-        price: number;
-        perChg: number;
+  const [watchlistPosition, setWatchlistPosition] = useState(0);
+
+  const stockSubset = (start: number): DisplayGroup[] => {
+    if (start < stocks.length - 5) {
+      return stocks.slice(start, start + 5);
+    } else {
+      return start === stocks.length
+        ? stocks.slice(0, 5)
+        : stocks.slice(start, stocks.length); // just make list shorter when they're at end, instead of wrapping around
     }
-    const stockSubset = (start: number): displayGroup[] => {
-        if (start < stocks.length - 5) {
-            return stocks.slice(start, start + 5);
-        } else {
-            return start === stocks.length
-                ? stocks.slice(0, 5)
-                : stocks.slice(start, stocks.length); // just make list shorter when they're at end, instead of wrapping around
-        }
-    };
-    function watchListPositionHandler(change: number) {
-        if (change === 5) {
-            if (watchlistPosition < stocks.length - 5) {
-                setWatchlistPosition(watchlistPosition + 5);
-            } else {
-                setWatchlistPosition(0); // loop around to beginning
-            }
-        } else {
-            if (watchlistPosition >= 5) {
-                setWatchlistPosition(watchlistPosition - 5);
-            } else if (watchlistPosition > 0 && watchlistPosition < 5) {
-                setWatchlistPosition(0);
-            } else {
-                setWatchlistPosition(stocks.length - 5);
-            }
-        }
+  };
+  function watchListPositionHandler(change: number) {
+    if (change === 5) {
+      if (watchlistPosition < stocks.length - 5) {
+        setWatchlistPosition(watchlistPosition + 5);
+      } else {
+        setWatchlistPosition(0); // loop around to beginning
+      }
+    } else {
+      if (watchlistPosition >= 5) {
+        setWatchlistPosition(watchlistPosition - 5);
+      } else if (watchlistPosition > 0 && watchlistPosition < 5) {
+        setWatchlistPosition(0);
+      } else {
+        setWatchlistPosition(stocks.length - 5);
+      }
     }
+  }
 
-    const [quote, setQuote] = useState("");
-    const [analystCalls, setAnalystCalls] = useState();
+  const [quote, setQuote] = useState("");
+  const [analystCalls, setAnalystCalls] = useState();
+  const [smallCard, setSmallCard] = useState();
 
-    const analystCallsHandler = async () => {
-        try {
-            const response = await fetch("http://127.0.0.1:8080/analystRec", {});
-            const json = await response.json();
+  const smallCardHandler = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8080/smallCard", {});
+      const json = await response.json();
 
-            setAnalystCalls(json);
-        } catch (err) {
-            console.log(err);
-        }
-    };
+      setSmallCard(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    const quoteHandler = async () => {
-        try {
-            const response = await fetch("http://127.0.0.1:8080/quote", {});
-            const json = await response.json();
+  const analystCallsHandler = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8080/analystRec", {});
+      const json = await response.json();
 
-            setQuote(json);
-        } catch (err) {
-            console.log(err);
-        }
-    };
+      setAnalystCalls(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    return (
-        <>
-            <Carousel
-                next={(_next, _active) => {
-                    watchListPositionHandler(5);
-                }}
-                prev={(prev, active) => {
-                    watchListPositionHandler(-5);
-                }}
-                autoPlay={false}
-                NextIcon={<ArrowForwardIosOutlinedIcon />}
-                PrevIcon={<ArrowBackIosNewOutlinedIcon />}
-                animation="slide"
-                // interval={14000}
-                navButtonsAlwaysVisible={true}
-                sx={{ width: "100%", height: "100%" }}
-            >
-                <StockGrouping displayGroup={stockSubset(watchlistPosition)} />
-            </Carousel>
-            <Box display="flex">
-                <Button onClick={quoteHandler}>Get quote</Button>
-                <Typography>{quote}</Typography>
-                <Button onClick={analystCallsHandler}>Get analyst calls</Button>
-                <Typography>{analystCalls}</Typography>
-            </Box>
-        </>
-    );
+  const quoteHandler = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8080/quote", {});
+      const json = await response.json();
+
+      setQuote(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <Carousel
+        next={(_next, _active) => {
+          watchListPositionHandler(5);
+        }}
+        prev={(prev, active) => {
+          watchListPositionHandler(-5);
+        }}
+        autoPlay={false}
+        NextIcon={<ArrowForwardIosOutlinedIcon />}
+        PrevIcon={<ArrowBackIosNewOutlinedIcon />}
+        animation="slide"
+        // interval={14000}
+        navButtonsAlwaysVisible={true}
+        sx={{ width: "100%", height: "100%" }}
+      >
+        <StockGrouping displayGroup={stockSubset(watchlistPosition)} />
+      </Carousel>
+      <Box display="flex">
+        <Button onClick={quoteHandler}>Get quote</Button>
+        <Typography>{quote}</Typography>
+        <Button onClick={analystCallsHandler}>Get analyst calls</Button>
+        <Typography>{analystCalls}</Typography>
+      </Box>
+    </>
+  );
 };
 
 export default Watchlist;
