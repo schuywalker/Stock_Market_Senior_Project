f345417964778b35d9e0e99d5da47100e63aae56
mszym99 <mszymanski202@gmail.com>
2023-03-10 16:34:30 -0800

    Database integration success

--- back-end/database.py
+++ back-end/database.py
@@ -1,36 +1,39 @@
+import hashlib
+import base64
 import mysql.connector
 from mysql.connector import Error
 #needs to be indiviuallized for each user and password
 #host, port, database are the same regardless of user
-mydb = mysql.connector.connect(
-    host="10.219.0.50",
-    port=3306,
-    user="root",
-    password="root",
-    database="SeniorProject_testConnection"    
-)
-#test connection to database
-if mydb.is_connected():
-    db_Info = mydb.get_server_info()
-    print("Connected to MySQL Server version ", db_Info)
-    cursor = mydb.cursor()
-    cursor.execute("select database();")
-    record = cursor.fetchone()
-    print("You're connected to database: ", record)
+try:
+    mydb = mysql.connector.connect(
+        host="10.219.0.50",
+        port=3306,
+        user="mszymanski",
+        password="Senior*2023",
+        database="SeniorProject_DB"    
+    )  
+   # If the connection was successful, print information about the connection
+    if mydb.is_connected():
+        db_Info = mydb.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = mydb.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
 
-#easier way to organize
-sql = "INSERT INTO SeniorProject_testConnection.SeniorProject_testTable (name, age) VALUES (%s, %s)"
-val = ("John", 36)
-#modular maybe?
-#insertOperator = cursor.execute(sql, val)
-#need to commit when inserting
+except Error as e:
+    print("Error while connecting to MySQL", e)
+    #Ultimately we will need to log failures with date and time
+    #this closes the database connection
+    mydb.close()
 
-#mydb.commit()
-
-cursor.execute("SELECT * FROM SeniorProject_testConnection.SeniorProject_testTable")
-#retrieves all rows associated and values
-for row in cursor.fetchall():
+cursor.execute("SELECT * FROM USERS")
+result = cursor.fetchall()
+for row in result:
     print(row)
 
+
 #this closes the database connection
-mydb.close()
+if mydb.is_connected():
+    mydb.close()
+    print("MySQL connection is closed")
