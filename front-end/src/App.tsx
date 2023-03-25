94e23cfd8195304a80a70b51467c1199ed9836a7
mlorhan <mlorhan@ewu.edu>
2023-03-24 19:53:33 -0700

    Add prettierrc for common formatting

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,45 +1,51 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import ResponsiveAppBar from "./scenes/global/AppBar";
-import ResponsiveSideBar from "./scenes/global/sidebar";
-import AnalystCalls from "./scenes/dashboard/AnalystCalls";
-import Watchlist from "./scenes/watchlist/Watchlist";
-import { ColorModeContext, useMode } from "./theme";
-import { Box, CssBaseline, PaletteMode, ThemeProvider } from "@mui/material";
-import { useState } from "react";
-import Stock from "./components/stock/Stock";
-import { ProSidebarProvider } from "react-pro-sidebar";
+import {Route, Routes} from 'react-router-dom'
+import './App.css'
+import ResponsiveAppBar from './scenes/global/AppBar'
+import ResponsiveSideBar from './scenes/global/sidebar'
+import AnalystCalls from './scenes/dashboard/AnalystCalls'
+import Watchlist from './scenes/watchlist/Watchlist'
+import {ColorModeContext, useMode} from './theme'
+import {Box, CssBaseline, PaletteMode, ThemeProvider} from '@mui/material'
+import {useState} from 'react'
+import Stock from './components/stock/Stock'
+import {ProSidebarProvider} from 'react-pro-sidebar'
 
 function App() {
-  const { theme, colorMode } = useMode();
+    const {theme, colorMode} = useMode()
 
-  return (
-    <>
-      {/* <ColorModeContext.Provider value = {{toggleColorMode}}> */}
-      <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <div className="App">
-            <div className="content">
-              <ResponsiveAppBar />
-              <Box sx={{ display: "flex", position: "relative" }}>
-                <ProSidebarProvider>
-                  <ResponsiveSideBar />
-                </ProSidebarProvider>
+    return (
+        <>
+            {/* <ColorModeContext.Provider value = {{toggleColorMode}}> */}
+            <ColorModeContext.Provider value={colorMode}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <div className="App">
+                        <div className="content">
+                            <ResponsiveAppBar />
+                            <Box sx={{display: 'flex', position: 'relative'}}>
+                                <ProSidebarProvider>
+                                    <ResponsiveSideBar />
+                                </ProSidebarProvider>
 
-                <Routes>
-                  {/* <Route path="/" element={<Home />}/> */}
-                  <Route path="/watchlist" element={<Watchlist />} />
-                  <Route path="/analyst-calls" element={<AnalystCalls />} />
-                  {/* <Route path="*" element={<NotFound />} /> */}
-                </Routes>
-              </Box>
-            </div>
-          </div>
-        </ThemeProvider>
-      </ColorModeContext.Provider>
-    </>
-  );
+                                <Routes>
+                                    {/* <Route path="/" element={<Home />}/> */}
+                                    <Route
+                                        path="/watchlist"
+                                        element={<Watchlist />}
+                                    />
+                                    <Route
+                                        path="/analyst-calls"
+                                        element={<AnalystCalls />}
+                                    />
+                                    {/* <Route path="*" element={<NotFound />} /> */}
+                                </Routes>
+                            </Box>
+                        </div>
+                    </div>
+                </ThemeProvider>
+            </ColorModeContext.Provider>
+        </>
+    )
 }
 
-export default App;
+export default App
--- front-end/src/scenes/dashboard/AnalystCalls.tsx
+++ front-end/src/scenes/dashboard/AnalystCalls.tsx
@@ -1,19 +1,20 @@
-import { Box, useTheme } from "@mui/material";
-import Button from "@mui/material/Button";
-import React, { useContext } from "react";
-import { ColorModeContext, tokens } from "../../theme";
-import AnalystCallTable from "../../components/table/AnalystCallTable";
-
+import {Box, useTheme} from '@mui/material'
+import Button from '@mui/material/Button'
+import React, {useContext} from 'react'
+import {ColorModeContext, tokens} from '../../theme'
+import AnalystCallTable from '../../components/table/AnalystCallTable'
 
 const AnalystCalls = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    const colorMode = useContext(ColorModeContext);
+    const theme = useTheme()
+    const colors = tokens(theme.palette.mode)
+    const colorMode = useContext(ColorModeContext)
     return (
         <>
-            <div><AnalystCallTable/></div>
+            <div>
+                <AnalystCallTable />
+            </div>
         </>
-    );
-};
+    )
+}
 
-export default AnalystCalls;
+export default AnalystCalls
