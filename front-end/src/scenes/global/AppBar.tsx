b7e2e6ec18472fd29656aa8c4d11012837163b73
schuyler <sasplin@ewu.edu>
2023-04-10 14:56:08 -0700

    remove margin -30 and unused imports

--- front-end/src/scenes/global/AppBar.tsx
+++ front-end/src/scenes/global/AppBar.tsx
@@ -1,23 +1,14 @@
-import * as React from 'react'
+import {useTheme} from '@mui/material'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
-import Menu from '@mui/material/Menu'
-import MenuIcon from '@mui/icons-material/Menu'
-import Container from '@mui/material/Container'
-import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
-import Tooltip from '@mui/material/Tooltip'
-import MenuItem from '@mui/material/MenuItem'
-import AdbIcon from '@mui/icons-material/Adb'
-import {Link} from 'react-router-dom'
-import SignUpForm from '../../components/SignUpForm'
-import {tokens} from '../../theme'
-import {useTheme} from '@mui/material'
+import Container from '@mui/material/Container'
+import Toolbar from '@mui/material/Toolbar'
+import * as React from 'react'
 import Cookies from 'universal-cookie'
 import LoginForm from '../../components/LoginForm'
+import SignUpForm from '../../components/SignUpForm'
+import {tokens} from '../../theme'
 
 const cookies = new Cookies()
 
@@ -40,10 +31,7 @@ function ResponsiveAppBar(props: any) {
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
-                <Toolbar
-                    disableGutters
-                    sx={{marginRight: -30, justifyContent: 'right'}}
-                >
+                <Toolbar disableGutters sx={{justifyContent: 'right'}}>
                     <Box sx={{display: 'flex', columnGap: 2}}>
                         <div style={{display: props.loggedIn ? 'none' : ''}}>
                             <Button
