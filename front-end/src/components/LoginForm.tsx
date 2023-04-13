ac48d4803eeb154ba53f9f0daa22aa2cb16f741b
Ocean Oestreicher <ooestreicher.ewu.edu>
2023-04-12 18:58:05 -0700

    Login Fix

--- front-end/src/components/LoginForm.tsx
+++ front-end/src/components/LoginForm.tsx
@@ -173,9 +173,9 @@ const cookies = new Cookies();
                             setUsernameHelperText("")
                             setPasswordTextFieldError(false)
                             setPasswordHelperText("")
-                            
+                            console.log(response)
             
-                            if(response.data['message']=='Invalid credentials'){
+                            if(response.data[0]['message']=='Invalid credentials'){
                               setUsernameValidated(false)
                               setUsernameTextFieldError(true)
                               setUsernameHelperText("Username doesn't exist")
