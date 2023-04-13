26af59bbb0199469884d0e2e289cbd06b6177172
schuyler <sasplin@ewu.edu>
2023-04-13 00:04:11 -0700

    Somehow this bugfix from Ocean got left out when I was trying to PR everything. Fixes everything - sorry about that!

--- front-end/src/components/LoginForm.tsx
+++ front-end/src/components/LoginForm.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, Modal, TextField, Typography, styled } from '@mui/material';
-import * as React from 'react';
-import axios from "axios";
-import Cookies from 'universal-cookie';
+import {Box, Button, Modal, TextField, Typography, styled} from '@mui/material'
+import * as React from 'react'
+import axios from 'axios'
+import Cookies from 'universal-cookie'
 import {backendBaseAddress} from '../config/globalVariables'
 
 const userEndPointConnection = axios.create({
-    baseURL: backendBaseAddress
+    baseURL: backendBaseAddress,
 })
 
 const style = {
@@ -17,186 +17,241 @@ const style = {
     p: 4,
 }
 const textfieldStyle = {
-  fontSize: 18,
+    fontSize: 18,
 }
 const CustomTextField = styled(TextField)({
-  '& label.Mui-focused': {
-    color: 'white',
-  },
-  '& label.Mui-error': {
-    color: '#f43414',
-  },
-  '& .MuiOutlinedInput-root': {
-    '&.Mui-focused fieldset': {
-      borderColor: 'white',
+    '& label.Mui-focused': {
+        color: 'white',
     },
-    '&.Mui-focused.Mui-error fieldset': {
-      borderColor: '#f43414',
+    '& label.Mui-error': {
+        color: '#f43414',
     },
-  },
-});
+    '& .MuiOutlinedInput-root': {
+        '&.Mui-focused fieldset': {
+            borderColor: 'white',
+        },
+        '&.Mui-focused.Mui-error fieldset': {
+            borderColor: '#f43414',
+        },
+    },
+})
 const helperTextStyle = {
-  fontSize: 12
+    fontSize: 12,
 }
 
-const cookies = new Cookies();
+const cookies = new Cookies()
 
-        export default function LoginForm(props: any){
-        const [username, setUsername] = React.useState("");
-        const [usernameValidated, setUsernameValidated] = React.useState(false);
-        const [usernameTextFieldError, setUsernameTextFieldError] = React.useState(false);
-        const [usernameHelperText, setUsernameHelperText] = React.useState("");
+export default function LoginForm(props: any) {
+    const [username, setUsername] = React.useState('')
+    const [usernameValidated, setUsernameValidated] = React.useState(false)
+    const [usernameTextFieldError, setUsernameTextFieldError] =
+        React.useState(false)
+    const [usernameHelperText, setUsernameHelperText] = React.useState('')
 
-        const [password, setPassword] = React.useState("");
-        const [passwordValidated, setPasswordValidated] = React.useState(false);
-        const [passwordTextFieldError, setPasswordTextFieldError] = React.useState(false);
-        const [passwordHelperText, setPasswordHelperText] = React.useState("");
+    const [password, setPassword] = React.useState('')
+    const [passwordValidated, setPasswordValidated] = React.useState(false)
+    const [passwordTextFieldError, setPasswordTextFieldError] =
+        React.useState(false)
+    const [passwordHelperText, setPasswordHelperText] = React.useState('')
 
-        const canSubmit = () =>{
-            if(usernameValidated && passwordValidated){
-                return true;
+    const canSubmit = () => {
+        if (usernameValidated && passwordValidated) {
+            return true
+        } else {
+            if (usernameValidated) {
+                setUsernameTextFieldError(false)
+                setUsernameHelperText('')
+            } else {
+                setUsernameTextFieldError(true)
+                if (usernameHelperText == '')
+                    setUsernameHelperText('Invalid Username Entered')
             }
-            else{
-                if(usernameValidated){
-                    setUsernameTextFieldError(false)
-                    setUsernameHelperText("")
-                }
-                else{
-                    setUsernameTextFieldError(true)
-                    if(usernameHelperText == "")setUsernameHelperText("Invalid Username Entered")
-                }
-                if(passwordValidated){
-                    setPasswordTextFieldError(false)
-                    setPasswordHelperText("")
-                }
-                else{
-                    setPasswordTextFieldError(true)
-                    if(passwordHelperText == "")setPasswordHelperText("Invalid Password Entered")
-                }
-            }
-            return false;
-        }
-        const validateUsername = (name:string)=>{
-            if(name.length > 0){
-                setUsernameValidated(true)
-            }
-            else{
-                setUsernameValidated(false)
+            if (passwordValidated) {
+                setPasswordTextFieldError(false)
+                setPasswordHelperText('')
+            } else {
+                setPasswordTextFieldError(true)
+                if (passwordHelperText == '')
+                    setPasswordHelperText('Invalid Password Entered')
             }
         }
-        const validatePassword = (password:string)=>{
-            if(password.length > 0){
-                setPasswordValidated(true)
-            }
-            else{
-                setPasswordValidated(false)
-            }
-        }
-
-        const resetFields = ()=>{
-            setUsernameTextFieldError(false)
+        return false
+    }
+    const validateUsername = (name: string) => {
+        if (name.length > 0) {
+            setUsernameValidated(true)
+        } else {
             setUsernameValidated(false)
-            setUsernameHelperText("")
-
-            setPasswordTextFieldError(false)
-            setPasswordValidated(false)
-            setPasswordHelperText("")
         }
-    
-    return(
+    }
+    const validatePassword = (password: string) => {
+        if (password.length > 0) {
+            setPasswordValidated(true)
+        } else {
+            setPasswordValidated(false)
+        }
+    }
+
+    const resetFields = () => {
+        setUsernameTextFieldError(false)
+        setUsernameValidated(false)
+        setUsernameHelperText('')
+
+        setPasswordTextFieldError(false)
+        setPasswordValidated(false)
+        setPasswordHelperText('')
+    }
+
+    return (
         <div>
             <Modal
-                sx = {style}
-                open= {props.open}
+                sx={style}
+                open={props.open}
                 onClose={props.close}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
-                hideBackdrop= {true}      >
-                <Box sx={{
-                    display:'flex',
-                    flexDirection: 'column',
-                    rowGap : 4,
-                    background : 'black',
-                    paddingTop: 1,
-                    paddingLeft : 3,
-                    paddingRight : 3,
-                    paddingBottom : 3
-                }}>
-                    
-                    <Box sx={{display: 'flex',flexDirection: 'row',justifyContent: 'space-between', alignItems:'center'}}>
-                        <Typography id="modal-modal-title" variant="h6" component="h2" sx={{fontSize: 18}}>
+                hideBackdrop={true}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        rowGap: 4,
+                        background: 'black',
+                        paddingTop: 1,
+                        paddingLeft: 3,
+                        paddingRight: 3,
+                        paddingBottom: 3,
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Typography
+                            id="modal-modal-title"
+                            variant="h6"
+                            component="h2"
+                            sx={{fontSize: 18}}
+                        >
                             Login
                         </Typography>
-                    <Button sx={{
-                    background: 'black',
-                    color: 'white',
-                    fontSize: 20,
-                    '&:hover':{
-                        background: 'black',
-                        color:'grey'
-                    },
-                    textAlign: 'right'
-                    }} 
-                    onClick={()=>{
-                    props.close()
-                    resetFields()
-                    }}>X</Button>
-                    </Box>   
-                    <CustomTextField InputProps={{sx: textfieldStyle}} InputLabelProps={{sx: textfieldStyle}} FormHelperTextProps={{sx: helperTextStyle}} id="username_field" label="Username" variant="outlined" required error = {usernameTextFieldError} helperText = {usernameHelperText}
-                        onChange = {(event)=>{
+                        <Button
+                            sx={{
+                                background: 'black',
+                                color: 'white',
+                                fontSize: 20,
+                                '&:hover': {
+                                    background: 'black',
+                                    color: 'grey',
+                                },
+                                textAlign: 'right',
+                            }}
+                            onClick={() => {
+                                props.close()
+                                resetFields()
+                            }}
+                        >
+                            X
+                        </Button>
+                    </Box>
+                    <CustomTextField
+                        InputProps={{sx: textfieldStyle}}
+                        InputLabelProps={{sx: textfieldStyle}}
+                        FormHelperTextProps={{sx: helperTextStyle}}
+                        id="username_field"
+                        label="Username"
+                        variant="outlined"
+                        required
+                        error={usernameTextFieldError}
+                        helperText={usernameHelperText}
+                        onChange={(event) => {
                             validateUsername(event.target.value)
-                            if(usernameValidated){
-                            setUsername(event.target.value)}
+                            if (usernameValidated) {
+                                setUsername(event.target.value)
                             }
-                            } 
-                            
+                        }}
                     />
-                    <CustomTextField type='password' InputProps={{sx: textfieldStyle}} InputLabelProps={{sx: textfieldStyle}} FormHelperTextProps={{sx: helperTextStyle}} id="password_field" label="Password" variant="outlined" required 
-                        error = {passwordTextFieldError} helperText = {passwordHelperText} onChange = {(event)=>{
+                    <CustomTextField
+                        type="password"
+                        InputProps={{sx: textfieldStyle}}
+                        InputLabelProps={{sx: textfieldStyle}}
+                        FormHelperTextProps={{sx: helperTextStyle}}
+                        id="password_field"
+                        label="Password"
+                        variant="outlined"
+                        required
+                        error={passwordTextFieldError}
+                        helperText={passwordHelperText}
+                        onChange={(event) => {
                             validatePassword(event.target.value)
-                            if(passwordValidated){
-                            setPassword(event.target.value)}
+                            if (passwordValidated) {
+                                setPassword(event.target.value)
                             }
-                            
-                    }/>
-                    <Button sx={{
-                        background: 'white',
-                        '&:hover':{
-                            background: 'grey',
-                            color:'white'
-                        }
-                    }}
-                    onClick={async()=>{
-                        if(canSubmit()){
-                          await userEndPointConnection.post("/userLogin?username="+username+"&password="+password).then((response)=>{
-                            setUsernameTextFieldError(false)
-                            setUsernameHelperText("")
-                            setPasswordTextFieldError(false)
-                            setPasswordHelperText("")
-            
-                            if(response.data[0]['message']=='Invalid credentials'){
-                              setUsernameValidated(false)
-                              setUsernameTextFieldError(true)
-                              setUsernameHelperText("Username doesn't exist")
+                        }}
+                    />
+                    <Button
+                        sx={{
+                            background: 'white',
+                            '&:hover': {
+                                background: 'grey',
+                                color: 'white',
+                            },
+                        }}
+                        onClick={async () => {
+                            if (canSubmit()) {
+                                await userEndPointConnection
+                                    .post(
+                                        '/userLogin?username=' +
+                                            username +
+                                            '&password=' +
+                                            password
+                                    )
+                                    .then((response) => {
+                                        setUsernameTextFieldError(false)
+                                        setUsernameHelperText('')
+                                        setPasswordTextFieldError(false)
+                                        setPasswordHelperText('')
+
+                                        if (
+                                            response.data[0]['message'] ==
+                                            'Invalid credentials'
+                                        ) {
+                                            setUsernameValidated(false)
+                                            setUsernameTextFieldError(true)
+                                            setUsernameHelperText(
+                                                "Username doesn't exist"
+                                            )
+                                        } else {
+                                            //login
+                                            cookies.set('user', username, {
+                                                path: '/',
+                                            })
+                                            cookies.set('password', password, {
+                                                path: '/',
+                                            })
+                                            cookies.set(
+                                                'user_id',
+                                                response.data['user_id']
+                                            )
+                                            props.close()
+                                            props.login()
+                                        }
+                                    })
+                                    .catch((e) => {
+                                        console.log(e)
+                                    })
                             }
-                            else{
-                              //login
-                              cookies.set("user",username,{ path: '/' })
-                              cookies.set("password",password,{ path: '/' })
-                              cookies.set("user_id",response.data[0]['user_id']);
-                              props.close()
-                              props.login()
-                            } 
-                          }).catch(e=>{
-                            console.log(e);
-                          })
-                        }
-                        
-                      }}>Submit</Button>    
+                        }}
+                    >
+                        Submit
+                    </Button>
                 </Box>
             </Modal>
         </div>
     )
-
-
 }
