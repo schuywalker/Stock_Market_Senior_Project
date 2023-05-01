b177e5ea6b0ba3df2b7e6f65b995e1736ca1c206
schuyler <sasplin@ewu.edu>
2023-04-30 23:31:58 -0700

    format changes for keys in financials

--- back-end/services/assetScreener.py
+++ back-end/services/assetScreener.py
@@ -21,81 +21,77 @@ class AssetScreenerService:
         ret = {
             "chartData": "yo",
             "basicInfo": {
-                "marketCap": round(basicFinancials.get("metric").get("marketCapitalization"), 2),
-                "dividendYield": basicFinancials.get("metric").get("dividendYieldIndicatedAnnual"),
-                "dividendGrowthRate5Y": basicFinancials.get("metric").get("dividendGrowthRate5Y"),
+                "market cap": round(basicFinancials.get("metric").get("marketCapitalization"), 2),
+                "dividend yield": basicFinancials.get("metric").get("dividendYieldIndicatedAnnual"),
+                "dividend growth rate 5Y": basicFinancials.get("metric").get("dividendGrowthRate5Y"),
             },
             "valuation": {
-                "peRatio": basicFinancials.get("metric").get("peExclExtraAnnual"),
-                "peRatioTTM": basicFinancials.get("metric").get("peBasicExclExtraTTM"),
+                "price/earnings FWD": basicFinancials.get("metric").get("peExclExtraAnnual"),
+                "price/earnings TTM": basicFinancials.get("metric").get("peBasicExclExtraTTM"),
 
-                "priceToSales": basicFinancials.get("metric").get("psAnnual"),
-                "priceToSalesTTM": basicFinancials.get("metric").get("psTTM"),
+                "price/sales": basicFinancials.get("metric").get("psAnnual"),
+                "price/sales TTM": basicFinancials.get("metric").get("psTTM"),
 
-                "pFreeCashFlowShareAnnual": basicFinancials.get("metric").get("pfcfShareAnnual"),
-                "pFreeCashFlowShareTTM": basicFinancials.get("metric").get("pfcfShareTTM"),
+                "price/free cash-flow": basicFinancials.get("metric").get("pfcfShareAnnual"),
+                "price/free cash-flow TTM": basicFinancials.get("metric").get("pfcfShareTTM"),
 
-                "pCashFlowShareTTM": basicFinancials.get("metric").get("pcfShareTTM"),
+                "price/free cash-flow TTM": basicFinancials.get("metric").get("pcfShareTTM"),
 
-                "psAnnual": basicFinancials.get("metric").get("psAnnual"),
-                "psTTM": basicFinancials.get("metric").get("psTTM"),
+                "price/sales": basicFinancials.get("metric").get("psAnnual"),
+                "price/sales TTM": basicFinancials.get("metric").get("psTTM"),
 
-                "priceToBook": basicFinancials.get("metric").get("ptbvAnnual"),
-                "pbAnnual": basicFinancials.get("metric").get("pbAnnual"),
+                "price/book": basicFinancials.get("metric").get("ptbvAnnual"),
+                "price/book": basicFinancials.get("metric").get("pbAnnual"),
 
-                "quickRatioAnnual": basicFinancials.get("metric").get("quickRatioAnnual"),
+                "quick ratio": basicFinancials.get("metric").get("quickRatioAnnual"),
             },
 
-            "priceMetrics": 24,
-            "financials": 25,
-            # "priceMetrics": {
-            #     "beta": basicFinancials.get("metric").get("beta"),
-            #     "priceReturnYTD": basicFinancials.get("metric").get("yearToDatePriceReturnDaily"),
-            #     "10DayAverageTradingVolume": basicFinancials.get("metric").get("10DayAverageTradingVolume"),
-            #     "52WeekHigh": basicFinancials.get("metric").get("52WeekHigh"),
-            #     "52WeekLow": basicFinancials.get("metric").get("52WeekLow"),
-            #     "yearToDatePriceReturnDaily": basicFinancials.get("metric").get("yearToDatePriceReturnDaily"),
-            #     "52WeekPriceReturnDaily": basicFinancials.get("metric").get("52WeekPriceReturnDaily"),
-            #     "5DayPriceReturnDaily": basicFinancials.get("metric").get("5DayPriceReturnDaily"),
-            #     "monthToDatePriceReturnDaily": basicFinancials.get("metric").get("monthToDatePriceReturnDaily"),
-            #     "priceRelativeToS&P50013Week": basicFinancials.get("metric").get("priceRelativeToS&P50013Week"), 
-            #     "priceRelativeToS&P50026Week": basicFinancials.get("metric").get("priceRelativeToS&P50026Week"), 
-            #     "priceRelativeToS&P5004Week": basicFinancials.get("metric").get("priceRelativeToS&P5004Week"), 
-            #     "priceRelativeToS&P500Ytd": basicFinancials.get("metric").get("priceRelativeToS&P500Ytd"), 
-
-            # },
-            # "financials": {
-            #     "cash_flow":{
-            #         "cashFlowPerShareAnnual": basicFinancials.get("metric").get("cashFlowPerShareAnnual"),
-            #         "cashFlowPerShareTTM": basicFinancials.get("metric").get("cashFlowPerShareTTM"),
-            #         "currentDividendYieldTTM": basicFinancials.get("metric").get("currentDividendYieldTTM"),
-            #     },
-            #     "income_statement":{
-            #         "netProfitMarginAnnual": basicFinancials.get("metric").get("netProfitMarginAnnual"),  
-            #         "grossMarginTTM": basicFinancials.get("metric").get("grossMarginTTM"),  
-            #         "epsGrowthTTMYoy": basicFinancials.get("metric").get("epsGrowthTTMYoy"),
-            #         "epsGrowth3Y": basicFinancials.get("metric").get("epsGrowth3Y"),
-            #         "epsGrowth5Y": basicFinancials.get("metric").get("epsGrowth5Y"),
-            #         "revenueGrowth5Y": basicFinancials.get("metric").get("revenueGrowth5Y"),
-
-            #         "netMarginGrowth5Y": basicFinancials.get("metric").get("netMarginGrowth5Y"),
-            #         "netProfitMargin5": basicFinancials.get("metric").get("netProfitMargin5"),
-            #         "netProfitMarginAnnual": basicFinancials.get("metric").get("netProfitMarginAnnual"),
-            #         "netProfitMarginTTM": basicFinancials.get("metric").get("netProfitMarginTTM"),
-            #         "operatingMargin5Y": basicFinancials.get("metric").get("operatingMargin5Y"),
-            #         "operatingMarginAnnual": basicFinancials.get("metric").get("operatingMarginAnnual"),
-            #         "operatingMarginTTM": basicFinancials.get("metric").get("operatingMarginTTM"),
-
-            #         "roeTTM": basicFinancials.get("metric").get("roeTTM"),
-            #     "roiAnnual": basicFinancials.get("metric").get("roiAnnual"),
-            #         "roiTTM": basicFinancials.get("metric").get("roiTTM"),
-            #     },
-            #     "balance_sheet":{
-            #         "longTermDebt/equityAnnual": basicFinancials.get("metric").get("longTermDebt/equityAnnual"),  
-            #         "totalDebt/totalEquityAnnual": basicFinancials.get("metric").get("totalDebt/totalEquityAnnual"),
-            #         "totalDebt/totalEquityQuarterly": basicFinancials.get("metric").get("totalDebt/totalEquityQuarterly"),
-            #     },
-            # }
-        }
+            "priceMetrics":{          
+                "beta": basicFinancials.get("metric").get("beta"),
+                "price return YTD": basicFinancials.get("metric").get("yearToDatePriceReturnDaily"),
+                "10 day average trading volume": basicFinancials.get("metric").get("10DayAverageTradingVolume"),
+                "52 week high": basicFinancials.get("metric").get("52WeekHigh"),
+                "52 week low": basicFinancials.get("metric").get("52WeekLow"),
+                "year to date price return daily": basicFinancials.get("metric").get("yearToDatePriceReturnDaily"),
+                "52 week price return daily": basicFinancials.get("metric").get("52WeekPriceReturnDaily"),
+                "5 day price return daily": basicFinancials.get("metric").get("5DayPriceReturnDaily"),
+                "month to date price return daily": basicFinancials.get("metric").get("monthToDatePriceReturnDaily"),
+                "price relative to S&P500 26 week": basicFinancials.get("metric").get("priceRelativeToS&P50026Week"), 
+                "price relative to S&P500 13 week": basicFinancials.get("metric").get("priceRelativeToS&P50013Week"), 
+                "price relative to S&P500 4 week": basicFinancials.get("metric").get("priceRelativeToS&P5004Week"), 
+                "price relative to S&P500 YTD": basicFinancials.get("metric").get("priceRelativeToS&P500Ytd"), 
+            },
+            "financials": {
+                "cash_flow":{
+                    "cash flow per share (annual)": basicFinancials.get("metric").get("cashFlowPerShareAnnual"),
+                    "cash flow per share TTM": basicFinancials.get("metric").get("cashFlowPerShareTTM"),
+                    "current dividend yield TTM": basicFinancials.get("metric").get("currentDividendYieldTTM"),
+                },
+                "income_statement":{
+                    "net profit margin (annual)": basicFinancials.get("metric").get("netProfitMarginAnnual"),  
+                    "gross margin TTM": basicFinancials.get("metric").get("grossMarginTTM"),  
+                    "EPS growth TTM YOY": basicFinancials.get("metric").get("epsGrowthTTMYoy"),
+                    "EPS growth 3Y": basicFinancials.get("metric").get("epsGrowth3Y"),
+                    "EPS growth 5Y": basicFinancials.get("metric").get("epsGrowth5Y"),
+                    "revenue growth 5Y": basicFinancials.get("metric").get("revenueGrowth5Y"),
+                    "net margin growth 5Y": basicFinancials.get("metric").get("netMarginGrowth5Y"),
+                    "net profit margin 5Y": basicFinancials.get("metric").get("netProfitMargin5"),
+                    # 5 or 5Y ^^^^ ?
+                    "net profit margin TTM": basicFinancials.get("metric").get("netProfitMarginTTM"),
+                    "operating margin 5Y": basicFinancials.get("metric").get("operatingMargin5Y"),
+                    "operating margin": basicFinancials.get("metric").get("operatingMarginAnnual"),
+                    "operating margin TTM": basicFinancials.get("metric").get("operatingMarginTTM"),
+                    "Return on Equity TTM": basicFinancials.get("metric").get("roeTTM"),
+                    "Return on Interest": basicFinancials.get("metric").get("roiAnnual"),
+                    "Return on Interest TTM": basicFinancials.get("metric").get("roiTTM"),
+                },
+                "balance_sheet":{
+                    "total debt/total equity": basicFinancials.get("metric").get("totalDebt/totalEquityAnnual"),
+                    "long term debt/equity": basicFinancials.get("metric").get("longTermDebt/equityAnnual"),  
+                    "total debt/total equity (quarterly)": basicFinancials.get("metric").get("totalDebt/totalEquityQuarterly"),
+                },
+            }
+            }
+        
         
         return ret
