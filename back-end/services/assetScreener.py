5a75d8c32ac1766249481f37eb99a6752272b790
schuyler <sasplin@ewu.edu>
2023-05-02 12:55:09 -0700

    format data :)

--- back-end/services/assetScreener.py
+++ back-end/services/assetScreener.py
@@ -1,3 +1,4 @@
+from services.utils.utils import serviceUtils
 import services.external_API_calls.finnhubCalls as fh
 from services.external_API_calls.finnhubCalls import *
 from config.database import db_controller
@@ -21,77 +22,78 @@ class AssetScreenerService:
         ret = {
             "chartData": "yo",
             "basicInfo": {
-                "market cap": round(basicFinancials.get("metric").get("marketCapitalization"), 2),
-                "dividend yield": basicFinancials.get("metric").get("dividendYieldIndicatedAnnual"),
-                "dividend growth rate 5Y": basicFinancials.get("metric").get("dividendGrowthRate5Y"),
+                "market cap": serviceUtils.formatResponse(basicFinancials.get("metric").get("marketCapitalization"), '$'),
+                "dividend yield": serviceUtils.formatResponse(basicFinancials.get("metric").get("dividendYieldIndicatedAnnual")),
+                "dividend growth rate 5Y": serviceUtils.formatResponse(basicFinancials.get("metric").get("dividendGrowthRate5Y")),
             },
             "valuation": {
-                "price/earnings FWD": basicFinancials.get("metric").get("peExclExtraAnnual"),
-                "price/earnings TTM": basicFinancials.get("metric").get("peBasicExclExtraTTM"),
+                "price/earnings FWD": serviceUtils.formatResponse( basicFinancials.get("metric").get("peExclExtraAnnual"),None),
+                "price/earnings TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("peBasicExclExtraTTM"), None),
 
-                "price/sales": basicFinancials.get("metric").get("psAnnual"),
-                "price/sales TTM": basicFinancials.get("metric").get("psTTM"),
+                "price/sales": serviceUtils.formatResponse(basicFinancials.get("metric").get("psAnnual"), None),
+                "price/sales TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("psTTM"), None),
 
-                "price/free cash-flow": basicFinancials.get("metric").get("pfcfShareAnnual"),
-                "price/free cash-flow TTM": basicFinancials.get("metric").get("pfcfShareTTM"),
+                "price/free cash-flow": serviceUtils.formatResponse(basicFinancials.get("metric").get("pfcfShareAnnual"), None),
+                "price/free cash-flow TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("pfcfShareTTM"), None),
 
-                "price/free cash-flow TTM": basicFinancials.get("metric").get("pcfShareTTM"),
+                "price/free cash-flow TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("pcfShareTTM"), None),
 
-                "price/sales": basicFinancials.get("metric").get("psAnnual"),
-                "price/sales TTM": basicFinancials.get("metric").get("psTTM"),
+                "price/sales": serviceUtils.formatResponse(basicFinancials.get("metric").get("psAnnual"), None),
+                "price/sales TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("psTTM"), None),
 
-                "price/book": basicFinancials.get("metric").get("ptbvAnnual"),
-                "price/book": basicFinancials.get("metric").get("pbAnnual"),
+                "price/book": serviceUtils.formatResponse(basicFinancials.get("metric").get("ptbvAnnual"), None),
+                "price/book": serviceUtils.formatResponse(basicFinancials.get("metric").get("pbAnnual"), None),
 
-                "quick ratio": basicFinancials.get("metric").get("quickRatioAnnual"),
+                "quick ratio": serviceUtils.formatResponse(basicFinancials.get("metric").get("quickRatioAnnual"), None),
             },
 
             "priceMetrics":{          
-                "beta": basicFinancials.get("metric").get("beta"),
-                "price return YTD": basicFinancials.get("metric").get("yearToDatePriceReturnDaily"),
-                "10 day average trading volume": basicFinancials.get("metric").get("10DayAverageTradingVolume"),
-                "52 week high": basicFinancials.get("metric").get("52WeekHigh"),
-                "52 week low": basicFinancials.get("metric").get("52WeekLow"),
-                "year to date price return daily": basicFinancials.get("metric").get("yearToDatePriceReturnDaily"),
-                "52 week price return daily": basicFinancials.get("metric").get("52WeekPriceReturnDaily"),
-                "5 day price return daily": basicFinancials.get("metric").get("5DayPriceReturnDaily"),
-                "month to date price return daily": basicFinancials.get("metric").get("monthToDatePriceReturnDaily"),
-                "price relative to S&P500 26 week": basicFinancials.get("metric").get("priceRelativeToS&P50026Week"), 
-                "price relative to S&P500 13 week": basicFinancials.get("metric").get("priceRelativeToS&P50013Week"), 
-                "price relative to S&P500 4 week": basicFinancials.get("metric").get("priceRelativeToS&P5004Week"), 
-                "price relative to S&P500 YTD": basicFinancials.get("metric").get("priceRelativeToS&P500Ytd"), 
+                "beta": serviceUtils.formatResponse(basicFinancials.get("metric").get("beta"), None),
+                "price return YTD": serviceUtils.formatResponse( basicFinancials.get("metric").get("yearToDatePriceReturnDaily")),
+                "10 day average trading volume": serviceUtils.formatResponse(basicFinancials.get("metric").get("10DayAverageTradingVolume")), # is it in $ amount???
+                "52 week high": serviceUtils.formatResponse(basicFinancials.get("metric").get("52WeekHigh"), '$'),
+                "52 week low": serviceUtils.formatResponse(basicFinancials.get("metric").get("52WeekLow"), '$'),
+                "year to date price return": serviceUtils.formatResponse(basicFinancials.get("metric").get("yearToDatePriceReturnDaily")),
+                "52 week price return": serviceUtils.formatResponse(basicFinancials.get("metric").get("52WeekPriceReturnDaily")),
+                "5 day price return": serviceUtils.formatResponse(basicFinancials.get("metric").get("5DayPriceReturnDaily")),
+                "month to date price return": serviceUtils.formatResponse(basicFinancials.get("metric").get("monthToDatePriceReturnDaily")),
+                "price relative to S&P500 26 week": serviceUtils.formatResponse(basicFinancials.get("metric").get("priceRelativeToS&P50026Week")), 
+                "price relative to S&P500 13 week": serviceUtils.formatResponse(basicFinancials.get("metric").get("priceRelativeToS&P50013Week")), 
+                "price relative to S&P500 4 week": serviceUtils.formatResponse(basicFinancials.get("metric").get("priceRelativeToS&P5004Week")), 
+                "price relative to S&P500 YTD": serviceUtils.formatResponse(basicFinancials.get("metric").get("priceRelativeToS&P500Ytd")), 
             },
             "financials": {
                 "cash_flow":{
-                    "cash flow per share (annual)": basicFinancials.get("metric").get("cashFlowPerShareAnnual"),
-                    "cash flow per share TTM": basicFinancials.get("metric").get("cashFlowPerShareTTM"),
-                    "current dividend yield TTM": basicFinancials.get("metric").get("currentDividendYieldTTM"),
+                    "cash flow per share (annual)": serviceUtils.formatResponse(basicFinancials.get("metric").get("cashFlowPerShareAnnual"), '$'),
+                    "cash flow per share TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("cashFlowPerShareTTM"), '$'),
+                    "current dividend yield TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("currentDividendYieldTTM")),
                 },
                 "income_statement":{
-                    "net profit margin (annual)": basicFinancials.get("metric").get("netProfitMarginAnnual"),  
-                    "gross margin TTM": basicFinancials.get("metric").get("grossMarginTTM"),  
-                    "EPS growth TTM YOY": basicFinancials.get("metric").get("epsGrowthTTMYoy"),
-                    "EPS growth 3Y": basicFinancials.get("metric").get("epsGrowth3Y"),
-                    "EPS growth 5Y": basicFinancials.get("metric").get("epsGrowth5Y"),
-                    "revenue growth 5Y": basicFinancials.get("metric").get("revenueGrowth5Y"),
-                    "net margin growth 5Y": basicFinancials.get("metric").get("netMarginGrowth5Y"),
-                    "net profit margin 5Y": basicFinancials.get("metric").get("netProfitMargin5"),
+                    "net profit margin (annual)": serviceUtils.formatResponse(basicFinancials.get("metric").get("netProfitMarginAnnual")),  
+                    "gross margin TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("grossMarginTTM")),  
+                    "EPS growth TTM YOY": serviceUtils.formatResponse(basicFinancials.get("metric").get("epsGrowthTTMYoy")),
+                    "EPS growth 3Y": serviceUtils.formatResponse(basicFinancials.get("metric").get("epsGrowth3Y")),
+                    "EPS growth 5Y": serviceUtils.formatResponse(basicFinancials.get("metric").get("epsGrowth5Y")),
+                    "revenue growth 5Y": serviceUtils.formatResponse(basicFinancials.get("metric").get("revenueGrowth5Y")),
+                    "net margin growth 5Y": serviceUtils.formatResponse(basicFinancials.get("metric").get("netMarginGrowth5Y")),
+                    "net profit margin 5Y": serviceUtils.formatResponse(basicFinancials.get("metric").get("netProfitMargin5")),
                     # 5 or 5Y ^^^^ ?
-                    "net profit margin TTM": basicFinancials.get("metric").get("netProfitMarginTTM"),
-                    "operating margin 5Y": basicFinancials.get("metric").get("operatingMargin5Y"),
-                    "operating margin": basicFinancials.get("metric").get("operatingMarginAnnual"),
-                    "operating margin TTM": basicFinancials.get("metric").get("operatingMarginTTM"),
-                    "Return on Equity TTM": basicFinancials.get("metric").get("roeTTM"),
-                    "Return on Interest": basicFinancials.get("metric").get("roiAnnual"),
-                    "Return on Interest TTM": basicFinancials.get("metric").get("roiTTM"),
+                    "net profit margin TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("netProfitMarginTTM")),
+                    "operating margin 5Y": serviceUtils.formatResponse(basicFinancials.get("metric").get("operatingMargin5Y")),
+                    "operating margin": serviceUtils.formatResponse(basicFinancials.get("metric").get("operatingMarginAnnual")),
+                    "operating margin TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("operatingMarginTTM")),
+                    "Return on Equity TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("roeTTM")),
+                    "Return on Interest": serviceUtils.formatResponse(basicFinancials.get("metric").get("roiAnnual")),
+                    "Return on Interest TTM": serviceUtils.formatResponse(basicFinancials.get("metric").get("roiTTM")),
                 },
                 "balance_sheet":{
-                    "total debt/total equity": basicFinancials.get("metric").get("totalDebt/totalEquityAnnual"),
-                    "long term debt/equity": basicFinancials.get("metric").get("longTermDebt/equityAnnual"),  
-                    "total debt/total equity (quarterly)": basicFinancials.get("metric").get("totalDebt/totalEquityQuarterly"),
+                    "total debt/total equity": serviceUtils.formatResponse(basicFinancials.get("metric").get("totalDebt/totalEquityAnnual"), None),
+                    "long term debt/equity": serviceUtils.formatResponse(basicFinancials.get("metric").get("longTermDebt/equityAnnual"), None),  
+                    "total debt/total equity (quarterly)": serviceUtils.formatResponse(basicFinancials.get("metric").get("totalDebt/totalEquityQuarterly"), None),
                 },
             }
             }
         
         
         return ret
+
---
+++ back-end/services/utils/utils.py
@@ -0,0 +1,14 @@
+
+
+class serviceUtils:
+    @staticmethod
+    def formatResponse(response, format='%',  decimals=2,) -> str:
+        if response is None:
+            return 'None'
+        if format == '$':
+            return '$ ' + str(round(response, decimals)) if float(response < 1000) else '$ ' + str(round(response, decimals)) + 'todo: get commas figured out'
+
+        elif format == '%':
+            return str(round(response, decimals)) + ' %'
+        else:
+            return str(round(response, decimals))
